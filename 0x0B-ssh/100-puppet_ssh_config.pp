71cd92cffdcd9f374761bc130063ef1b55a4d962
LevittC17 <LeewednesdayC17@protonmail.com>
2023-06-27 20:50:11 +0000

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
 # Client configuration file (w/ Puppet)
 
-file_line { 'ssh_config':
-  path    => '/etc/ssh/ssh_config',
-  line    => [
-    Host 122750-web-01,
-        HostName 18.207.140.105,
-        User ubuntu,
-        IdentityFile ~/.ssh/school,
-        PreferredAuthentications publickey,
-        PasswordAuthentication no,
-  ],
-  ensure  => present,
-  match   => ^Host 122750-web-01$,
+file_line { 'NoPassword':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no'
+}
+
+file_line { 'DefineConfigFile':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
 }
