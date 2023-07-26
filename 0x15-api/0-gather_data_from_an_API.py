dbb8e58d8bec77615091d7efa7c68bc33ec5d61d
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-26 21:05:26 +0000

    get employee id and return their information todo list progress: REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,50 +10,40 @@ import requests
 import sys
 
 
-def get_employee_todo_list(employee_id):
-    """Get REST API URLsm employee ID and task url """
+def get_employee_todo_list_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
 
     try:
-        # Fetch user data
-        response_user = requests.get(user_url)
-        response_user.raise_for_status()
-        user_data = response_user.json()
-        employee_name = user_data["name"]
+        user_response = requests.get(user_url)
+        todos_response = requests.get(todos_url)
+        user_response.raise_for_status()
+        todos_response.raise_for_status()
 
-        # Fetch todo list data
-        response_todo = requests.get(todo_url)
-        response_todo.raise_for_status()
-        todo_data = response_todo.json()
+        user_data = user_response.json()
+        todos_data = todos_response.json()
 
-        # Calculate the number of done tasks
-        done_tasks = [task for task in todo_data if task["completed"]]
-        number_of_done_tasks = len(done_tasks)
-        total_number_of_tasks = len(todo_data)
+        empName = user_data["name"]
+        totTasks = len(todos_data)
+        doneTasks = sum(1 for todo in todos_data if todo["completed"])
 
-        # Display the results
-        print(f"Employee {employee_name} is done with
-              tasks({number_of_done_tasks}/{total_number_of_tasks}): ")
-        for task in done_tasks:
-            print(f"\t{task['title']}")
-
+        print(f"Employee {empName} done with tasks({doneTasks}/{totTasks}):")
+        for todo in todos_data:
+            if todo["completed"]:
+                print(f"\t{todo['title']}")
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         sys.exit(1)
     except KeyError:
-        print(f"Error: Invalid employee ID ({employee_id}).")
-        sys.exit(1)
-    except ValueError:
-        print(f"Error: Invalid JSON response from the API.")
+        print("Error: Employee not found.")
         sys.exit(1)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Please provide a valid employee ID as a parameter.")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
-    get_employee_todo_list(employee_id)
+    get_employee_todo_list_progress(employee_id)
