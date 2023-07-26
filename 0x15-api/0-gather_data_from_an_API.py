94ee004b10ec14684543c7e347d401d4999e66e7
LevittC17 <LeewednesdayC17@protonmail.com>
2023-07-26 10:34:43 +0000

    employee info about a todo list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,21 +17,20 @@ def employee_todo_list(employee_id):
 
     try:
         user_response = requests.get(user_url)
-	todos_response = requests.get(todos_url)
+        todos_response = requests.get(todos_url)
 
-	user_data = user_response.json()
-	todos_data = todos_response.json()
+        user_data = user_response.json()
+        todos_data = todos_response.json()
 
-	employee_name = user_data['name']
-	total_tasks = len(todos_data)
-	done_tasks = sum(1 for task in todos_data if task['completed']
+        employee_name = user_data['name']
+        total_tasks = len(todos_data)
+        done_tasks = sum(1 for task in todos_data if task['completed']
 
         print(f'Employee {employee_name} is done with tasks({done_tasks} /
-	      {total_tasks}): ')
-
-	for task in todos_data:
-	    if task['completed']:
-	        print(f'\t{task["title"]}')
+                         {total_tasks}): ')
+        for task in todos_data:
+            if task['completed']:
+            print(f'\t{task["title"]}')
 
     except requests.exceptions.RequestException as e:
         print('Error occurred while fethcing data from the API:', e)
@@ -42,4 +41,4 @@ if __name__ == "__main__":
         print('Usage: python3 gather_data_from_an_API.py <employee_id>')
     else:
         employee_id=int(sys.argv[1])
-	employee_todo_list(employee_id)
+        employee_todo_list(employee_id)
