6d1f6fd8f66f47edc0a1e26591bcc67b3bea832a
LevittC17 <LeewednesdayC17@protonmail.com>
2023-08-08 21:45:56 +0000

    Query Reddit API - parse title of all hot articles - print sorted vount of given keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -8,64 +8,52 @@ keywords (case-insensitive, delimited by spaces
 
 
 import requests
-import re
-from collections import Counter
 
 
-def count_words(subreddit, word_list, after=None):
-    # Initialize the Counter
-    word_counter = Counter()
+def count_words(subreddit, word_list, after='', word_dict=None):
+    if word_dict is None:
+        word_dict = {word.lower(): 0 for word in word_list}
 
-    # Set a custom User-Agent header to avoid Too Many Requests errors
-    headers = {'User-Agent': 'My Reddit Bot'}
+    if after is None:
+        print_counts(word_dict)
+        return None
 
-    # Construct the API URL for the subreddit's hot posts
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {'after': after} if after else None
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {'User-Agent': 'my_custom_user_agent'}
+    params = {'limit': 100, 'after': after}
 
     try:
-        response = requests.get(url, headers=headers, params=params)
-        # Raise an exception if the response status code is not 200
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
         response.raise_for_status()
-        data = response.json()
+        data = response.json().get('data', {})
+        hot_posts = data.get('children', [])
+        new_after = data.get('after')
 
-        # Check if the response contains a 'data' field
-        if 'data' in data and 'children' in data['data']:
-            posts = data['data']['children']
-            for post in posts:
-                title = post['data']['title']
-                for word in word_list:
-                    # Use regular expression to match whole words only
-                    word = word.lower()
-                    pattern = rf'\b{re.escape(word)}\b'
-                    if re.search(pattern, title.lower()):
-                        word_counter[word] += 1
+        for post in hot_posts:
+            title = post['data']['title'].lower()
+            for word in word_dict:
+                word_dict[word] += title.count(word)
 
-            # Check for pagination (next page)
-            after = data['data']['after']
-            if after is not None:
-                # Recursively call the function with the next page
-                count_words(subreddit, word_list, after)
-
-        sorted_counts = sorted(word_counter.items(),
-                               key=lambda item: (-item[1], item[0]))
-
-        for word, count in sorted_counts:
-            print(f"{word}: {count}")
-
-    except (requests.RequestException,
-            requests.HTTPError, requests.ConnectionError) as e:
+        count_words(subreddit, word_list, new_after, word_dict)
+    except requests.exceptions.RequestException as e:
         print("An error occurred:", e)
 
 
-if __name__ == "__main__":
-    subreddit_name = sys.argv[1] if len(sys.argv) > 1 else None
-    word_list = sys.argv[2:] if len(sys.argv) > 2 else []
+def print_counts(word_dict):
+    sorted_counts = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
+    for word, count in sorted_counts:
+        if count:
+            print(f"{word}: {count}")
 
-    if subreddit_name and word_list:
-        count_words(subreddit_name, word_list)
-    else:
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print(
-            "Ex: {} programming 'python java javascript'".format(
-                sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'"
+              .format(sys.argv[0]))
+    else:
+        subreddit_name = sys.argv[1]
+        word_list = sys.argv[2:]
+        count_words(subreddit_name, word_list)
