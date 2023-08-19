ef883a5f0bc8dd61544bb0490f057bb6f5703aa3
LevittC17 <LeewednesdayC17@protonmail.com>
2023-08-19 09:26:53 +0000

    hard / soft file limit increase

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# user holberton to access the docker container
+# hard file limit increase
+exec { 'holberton-user hard-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# soft file limit increase
+exec { 'holberton-user soft-file':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
